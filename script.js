b73aadbc2df8688c7fd008b9c73115dfbe476fec
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-09-17 16:11:44 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//get a function that fetches the API and saves it to a list then we run the rest of the function from that 
+//function.
+
+let closeBtn = document.getElementById('closeBtn');
+
+let remove = e => e.target.parentElement.remove();
